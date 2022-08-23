79a9a4dcfceaa6c0122cb063c043ea8d63e2e322
Serhii Kushneruk <svkushneruk@ukr.net>
2022-08-23 21:33:18 +0300

    Update NotFoundView.jsx

--- src/views/NotFoundView/NotFoundView.jsx
+++ src/views/NotFoundView/NotFoundView.jsx
@@ -1,5 +1,10 @@
 import { Wrapper } from './NotFoundView.styled';
+import { Link } from 'react-router-dom';
 
 export const NotFoundView = () => {
-  return <Wrapper>Error 404. Page not found</Wrapper>;
+  return (
+    <Wrapper>
+      Error 404. Page not found. Back to <Link to="/">home page</Link>{' '}
+    </Wrapper>
+  );
 };
