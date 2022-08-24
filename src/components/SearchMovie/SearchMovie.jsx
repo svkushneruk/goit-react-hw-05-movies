a8cd9c67d91e738bd96dc6a94f200f80a418996f
Serhii Kushneruk <45001549+svkushneruk@users.noreply.github.com>
2022-08-24 13:13:18 +0300

    add search by keyword

--- src/components/SearchMovie/SearchMovie.jsx
+++ src/components/SearchMovie/SearchMovie.jsx
@@ -5,32 +5,44 @@ import {
   SearchBtn,
 } from 'components/SearchMovie/SearchMovie.styled';
 import * as API from 'services/MovieAPI';
+import { useSearchParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { TrandingList, MovieLink } from 'views/HomeView/HomeView.styled';
 
 export const SearchMovie = () => {
-  const [query, setQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useSearchParams();
   const [searchList, setSearchList] = useState(null);
-
-  const handleChange = e => {
-    setQuery(e.target.value);
-  };
+  const keyword = searchQuery.get('query') ?? '';
 
   const handleFormSubmit = e => {
     e.preventDefault();
-    if (query.trim() === '') {
+    setSearchQuery({ query: e.target.query.value });
+  };
+
+  useEffect(() => {
+    if (keyword.trim() === '') {
       return;
     }
+    API.getMoviesByKeyword(keyword).then(setSearchList);
+  }, [keyword]);
 
-    API.getMoviesByKeyword(query).then(setSearchList);
-  };
   return (
-    <SearchForm onSubmit={handleFormSubmit}>
-      <SearchInput
-        type="text"
-        name="query"
-        onChange={handleChange}
-        value={query}
-      />
-      <SearchBtn type="submit">Search</SearchBtn>
-    </SearchForm>
+    <div>
+      <SearchForm onSubmit={handleFormSubmit}>
+        <SearchInput type="text" name="query" />
+        <SearchBtn type="submit">Search</SearchBtn>
+      </SearchForm>
+      {searchList && keyword && (
+        <TrandingList>
+          {searchList.map(movie => {
+            return (
+              <li key={movie.id}>
+                <MovieLink to={`/movies/${movie.id}`}>{movie.title}</MovieLink>
+              </li>
+            );
+          })}
+        </TrandingList>
+      )}
+    </div>
   );
 };
